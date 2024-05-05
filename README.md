6ca98ed5fd71fb9e23d1443e63867095bc124e23
Giovanni <gvn.gentile@gmail.com>
2024-05-04 22:04:03 -0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# The Vital Message
+
+## About the game
+
+This program is a reproduction of the game "The Vital Message," published in the book "Computer Battlegames" by Usborne Computer Games in 1982. 
+
+I became aware of this book and the game through the YouTube channel of the creator Kari Lawler (https://www.youtube.com/@karilawler), who programmed this game in the following video: https://www.youtube.com/watch?v=3kdM9wyglnw.
+
+This is my rendition of the game, which basically uses the same logic and commands used by Kari in the video, but with two significant differences:
+- Dividing the game logic into smaller functions
+- Changing variable names
+
+In the video, the creator opted to remain faithful to the book version by naming variables and adhering to the program logic as instructed in the book. This enhances the fidelity of her algorithm.
+
+This is the program description as stated in the book "Computer Battlegames":
+
+> You are a laser communications operator. Your job is to intercept robot messages and relay them to Command HQ. A vital code message is expected. If you relay it correctly, the Robot attack will be crushed. This game tests your skill at remembering a group of letters which you see for only a very short time. The computer will ask you for a difficulty from 4 to 10. When you have typed in your answer, letters will appear top left of your screen and disappear again fairly quickly. Memorize them and then type them into the computer and see if you were right. 
---
+++ the_vital_message.py
@@ -0,0 +1,62 @@
+import os
+import random
+import string
+import time
+
+
+def set_difficult():
+    valid_input = False
+    MINIMUM_DIFFICULT = 4
+    MAXIMUM_DIFFICULT = 10
+
+    while valid_input is not True:
+        try:
+            difficult = int(input('HOW DIFFICULT? (4-10) '
+                                  '\n'))
+            if difficult >= MINIMUM_DIFFICULT and difficult <= MAXIMUM_DIFFICULT:
+                valid_input = True
+        except(ValueError):
+            print('ONLY NUMBERS ALLOWED')
+
+    return difficult
+
+
+def intercept_message(difficult):
+    intercepted_message = ''
+
+    for i in range(difficult):
+        intercepted_message += random.choice(string.ascii_uppercase)
+
+    return intercepted_message
+
+
+def send_intercepted_message(difficult):
+    time.sleep(0.5 * difficult)
+    os.system('cls||clear')
+
+    return input('')
+
+def verify_intercepted_message(intercepted_message, sent_message):
+    if intercepted_message == sent_message:
+        print('MESSAGE CORRECT'
+              '\n'
+              'THE WAR IS OVER')
+    else:
+        print('YOU GOT IT WRONG')
+        print('YOU SHOULD HAVE SENT: '
+              '\n'
+              + intercepted_message)
+
+
+if __name__ == '__main__':
+    print('THE VITAL MESSAGE')
+
+    difficult = set_difficult()
+    intercepted_message = intercept_message(difficult)
+
+    print('SEND THIS MESSAGE:'
+          '\n'
+          + intercepted_message)
+
+    sent_message = send_intercepted_message(difficult)
+    verify_intercepted_message(intercepted_message, sent_message)
